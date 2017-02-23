44e81eda6f6f89de46f4b5c434001dee9bcbc2cc
Franco Abaroa <franco9395@gmail.com>
2017-02-22 17:10:19 -0800

    Add functions review

---
+++ functionsWarmUp.js
@@ -0,0 +1,9 @@
+/********* Functions Review *********/
+
+/********* Open up your Chrome Browser and open up the console *********/
+
+// 1. Write a function that alerts the message "This function is working!" when called (invoked, run, etc)
+
+// 2. Write a function that accepts a color as an argument (parameter of the function) and logs the following message to the console: "My favorite color is " + color + "!"
+
+// 3. Write a function that accepts a number as an argument and returns that number + 12
