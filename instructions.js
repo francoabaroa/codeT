ebdf7245beb34eb3a4ffd257c2535e50dee57f7d
Franco Abaroa <franco9395@gmail.com>
2017-02-23 16:20:14 -0800

    Add new assignment

---
+++ instructions.js
@@ -0,0 +1,10 @@
+/** INSTRUCTIONS **/
+
+
+/** Go to www.codeacademy.com and create an account if you don't have one. Log in if you do. **/
+
+
+/** Go to www.codecademy.com/learn/javascript and next to 'Overview', click on 'Syllabus'. **/
+
+
+/** Click on section 2 titled Functions (skip Introduction to Javascript) and click on 'Introduction to Functions'. Work through the entire Functions section and notify us when you are done with it. **/
